f5a3b5c90d63ab4c61609fb84a736a22b383bb5e
Manuel <manuel@vikingosegundo.de>
2014-09-09 16:11:02 +0200

    fixed parameter type

--- WebViewProxy/WebViewProxy.h
+++ WebViewProxy/WebViewProxy.h
@@ -44,7 +44,7 @@ typedef void (^WVPHandler)(NSURLRequest* req, WVPResponse* res);
 + (void) handleRequestsWithHost:(NSString*)host pathPrefix:(NSString*)pathPrefix handler:(WVPHandler)handler;
 + (void) handleRequestsMatching:(NSPredicate*)predicate handler:(WVPHandler)handler;
 
-+ (void) handleRequestsMatchingTest:(BOOL (^)(id request)) testBlock
++ (void) handleRequestsMatchingTest:(BOOL (^)(NSURL *url)) testBlock
                             handler:(WVPHandler)handler;
 
 @end
